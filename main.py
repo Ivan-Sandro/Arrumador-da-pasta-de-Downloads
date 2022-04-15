bbe6148764fc8c62398eea4e47052721a6e37338
Ivan Sandro <81819218+Ivan-Sandro@users.noreply.github.com>
2022-04-15 17:58:10 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from shutil import move as moverArquivoDePasta
 import SystemPath as sp
 
-for arquivo in sp.pastaDownloads.glob('*'):
-    categoriaDoArquivo = sp.acharCategoriaArquivo(arquivo.suffix[1:])
-    
-    if categoriaDoArquivo != "None":
-        novoDiretorioDoArquivo = sp.Path(sp.pastaDownloads, categoriaDoArquivo)
+diretorios = sp.lerDiretoriosArquivo("diretorios.txt")
 
-        novoDiretorioDoArquivo.mkdir(parents=True, exist_ok=True)
-        moverArquivoDePasta(arquivo, novoDiretorioDoArquivo)
+for diretorio in diretorios:
+    if not diretorio.is_dir():
+        continue
+
+    for arquivo in diretorio.glob('*'):
+        categoriaDoArquivo = sp.acharCategoriaArquivo(arquivo.suffix[1:])
+        
+        if categoriaDoArquivo != "None":
+            novoDiretorioDoArquivo = sp.Path(diretorio, categoriaDoArquivo)
+
+            if not novoDiretorioDoArquivo.is_dir():
+                novoDiretorioDoArquivo.mkdir(parents=True, exist_ok=True)
+
+            if sp.checarArquivoRepetido(arquivo, novoDiretorioDoArquivo) == True:
+                arquivo = arquivo.rename(sp.Path(arquivo.parent, sp.getNovoNomeArquivoRepetido(arquivo, novoDiretorioDoArquivo)))
+
+            moverArquivoDePasta(arquivo, novoDiretorioDoArquivo)
